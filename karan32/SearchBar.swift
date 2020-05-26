85df2a374d510b27da479aef5594fb4a893bf841
starlessn1ght <41337686+starlessn1ght@users.noreply.github.com>
2020-05-26 16:06:58 -0300

    Create SearchBar.swift

---
+++ karan32/SearchBar.swift
@@ -0,0 +1,39 @@
+//
+//  SearchBar.swift
+//  karan32
+//
+//  Created by Helder on 26/05/20.
+//  Copyright © 2020 Helder de Melo Sérvio Filho. All rights reserved.
+//
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+    @Binding var text: String
+    var placeholder: String
+    class Coordinator: NSObject, UISearchBarDelegate{
+        @Binding var text: String
+        init(text: Binding<String>){
+            _text = text
+        }
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+            text = searchText
+        }
+    }
+    
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.placeholder = placeholder
+        searchBar.searchBarStyle = .minimal
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
